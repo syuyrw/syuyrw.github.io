5da9b53516efc48d9d0f5139931252e56abf10f1
syuyrw <92232875+syuyrw@users.noreply.github.com>
2022-05-25 22:28:40 -0600

    friday banner

--- Lesson-06/weather/js/friday-banner.js
+++ Lesson-06/weather/js/friday-banner.js
@@ -1,12 +1,6 @@
-
-  var weekday = new Array(7);
-
-  
-  weekday[5] = "Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
-
-  var currentDate = new Date();
-  weekdayValue = currentDate.getDay();
-
-  document.write(
-      weekday[weekdayValue]
-  );
+//Banner
+var cur_date = new Date()
+var weekday = cur_date.getDay()
+if (weekday != 5) {
+    document.getElementById("banner").style.display = "none"
+}
